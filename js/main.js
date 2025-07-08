ec28b859e08c6d75e0690e85ac28be6c572fbcba
Kashaf-ul-Eman21 <kashafeman20@gmail.com>
2025-07-08 13:17:17 +0500

    button
    Button Functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 for(let a=1; a<10; a++){
 document.writeln("<button>"+a+"</button>");
 }
+
+
+function myFunction(){
+    btn.style.backgroundColor=blue;
+}
+let btn=document.createElement("button");
+btn.innerText="click me!";
+btn.onclick(myFunction(),()=>{
+    
+});
+
