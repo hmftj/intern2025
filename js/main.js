4e2d4dd4c194b93669e737afb48aa8cfbcfc5dbf
KashafulEman <kashafeman20@gmail.com>
2025-07-08 13:40:45 +0500

    Update main.js
    changed button color

--- js/main.js
+++ js/main.js
@@ -2,13 +2,16 @@ for(let a=1; a<10; a++){
 document.writeln("<button>"+a+"</button>");
 }
 
-
-function myFunction(){
-    btn.style.backgroundColor=blue;
-}
 let btn=document.createElement("button");
 btn.innerText="click me!";
-btn.onclick(myFunction(),()=>{
-    
-});
+
+function myFunction(){
+    document.body.style.backgroundColor = ""; // Clears background
+    btn.style.backgroundColor="blue";
+}
+
+btn.onclick = myFunction;
+
+// Add the button to the page
+document.body.appendChild(btn);
 
