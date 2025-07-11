e52287d9b2a58648c19d711bb6e6fa548553a555
Kashaf-ul-Eman21 <kashafeman20@gmail.com>
2025-07-12 02:15:10 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
-for(let a=1; a<10; a++){
-document.writeln("<button>"+a+"</button>");
-}
+// for(let a=1; a<10; a++){
+// document.writeln("<button>"+a+"</button>");
+// }
 
-let btn=document.createElement("button");
-btn.innerText="click me!";
-
-
-function myFunction(){
-    document.body.style.backgroundColor = ""; // Clears background
-    btn.style.backgroundColor="blue";
-}
-
-btn.onclick = myFunction;
-
-// Add the button to the page
-document.body.appendChild(btn);
+// let btn=document.createElement("button");
+// btn.innerText="click me!";
 
 
-function myFunction(){
-    document.body.style.backgroundColor = ""; // Clears background
-    btn.style.backgroundColor="blue";
-}
+// function myFunction(){
+//     document.body.style.backgroundColor = ""; // Clears background
+//     btn.style.backgroundColor="blue";
+// }
 
-btn.onclick = myFunction;
+// btn.onclick = myFunction;
 
-// Add the button to the page
-document.body.appendChild(btn);
+// // Add the button to the page
+// document.body.appendChild(btn);
+
+
+// function myFunction(){
+//     document.body.style.backgroundColor = ""; // Clears background
+//     btn.style.backgroundColor="blue";
+// }
+
+// btn.onclick = myFunction;
+
+// // Add the button to the page
+// document.body.appendChild(btn);
